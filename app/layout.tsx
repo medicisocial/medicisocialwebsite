5ae5ac0b70fff2e832759ca53c7d7558788d1481
neverxpected <ibiss55@gmail.com>
2026-06-11 01:00:42 -0500

    refactor: replace next/script with standard script tag for Meta Pixel injection in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import SchemaOrg from './components/SchemaOrg'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import MotionProvider from './motion-provider'
-import Script from 'next/script'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -94,21 +93,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
-      <body className="font-sans">
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '433663773138457');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+      <head>
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '433663773138457');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
         <noscript>
           <img
             height="1"
@@ -118,6 +120,9 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+      <body className="font-sans">
         <MotionProvider>
           <Navbar />
           <SchemaOrg />
