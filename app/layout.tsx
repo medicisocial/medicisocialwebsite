4b2fc3b9b709281c82bf90f120daeb87d149d456
neverxpected <ibiss55@gmail.com>
2026-06-11 00:54:09 -0500

    feat: integrate Facebook Pixel script for tracking in main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import SchemaOrg from './components/SchemaOrg'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import MotionProvider from './motion-provider'
+import Script from 'next/script'
 
 const inter = Inter({
   subsets: ["latin"],
@@ -94,6 +95,29 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <body className="font-sans">
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '433663773138457');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=433663773138457&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <MotionProvider>
           <Navbar />
           <SchemaOrg />
